e4dfb3eacb0abe599d249f63a130065ceb28c073
daominhhieu97 <hieudm97@gmail.com>
2018-08-28 12:43:08 +0700

    New Challenging
    New Challenging

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        MULTICHOICEEntities db = new MULTICHOICEEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            dynamic myModel = new ExpandoObject();
+            myModel.lst_cauhoi = db.CAUHOIs.ToList<CAUHOI>();
+            myModel.lst_traloi = db.CAUTRALOIs.ToList<CAUTRALOI>();
+            return View(myModel);
+        }
+
+        [HttpPost]
+        public ActionResult Check(FormCollection fc)
+        {
+            int num_right = 0;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            initialDic(fc, dic);
+            num_right = howmanyright(num_right, dic);
+            ViewBag.num_right = num_right;
+            return View();
+        }
+
+        private int howmanyright(int num_right, Dictionary<int, int> dic)
+        {
+            foreach (KeyValuePair<int, int> entry in dic)
+            {
+                if (db.CAUHOIs.Where(cauhoi => cauhoi.ID == entry.Key).First().ID_CAUTRL == entry.Value)
+                    num_right += 1;
+            }
+
+            return num_right;
+        }
+
+        private void initialDic(FormCollection fc, Dictionary<int,int> dic)
+        {
+            for (int i = 1; i <= db.CAUHOIs.Count(); i++)
+            {
+                int key = i;
+                int value = int.Parse(fc["cauhoi" + i.ToString()]);
+                dic.Add(key, value);
+            }
+        }
+    }
+}
---
+++ WebApplication3/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class LoginController : Controller
+    {
+        MULTICHOICEEntities db = new MULTICHOICEEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ToHome(FormCollection user)
+        {
+            USER u = new USER
+            {
+                USERNAME = user["USERNAME"],
+                PASSWORD = user["PASSWORD"]
+            };
+            if(CheckUser(u))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [NonAction]
+        public Boolean CheckUser(USER user)
+        {
+            if(db.USERs.Where(u => u.USERNAME == user.USERNAME).First()!= null)
+                return true;
+            return false;
+
+        }
+    }
+}
