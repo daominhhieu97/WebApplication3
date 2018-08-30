921f0320923942880dc87240b0131e04f73be5a7
daominhhieu97 <hieudm97@gmail.com>
2018-08-30 08:19:29 +0700

    DAY 2
    Hoàn thành view : trang chủ và các page con.

--- WebBook/Controllers/HomePageController.cs
+++ WebBook/Controllers/HomePageController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBook.Models;
 
 namespace WebBook.Controllers
 {
     public class HomePageController : Controller
     {
+        BookShopEntities db = new BookShopEntities();
         // GET: HomePage
         public ActionResult Index()
+        {
+            List<Sach> lst_sachs = db.Saches.ToList();
+            return View(lst_sachs);
+        }
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+        public ActionResult LienHe()
         {
             return View();
         }
--- WebBook/Controllers/LoginPageController.cs
+++ WebBook/Controllers/LoginPageController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebBook.Models;
 namespace WebBook.Controllers
 {
     public class LoginPageController : Controller
     {
+        BookShopEntities db = new BookShopEntities();
         // GET: LoginPage
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CheckAuthen(NguoiDung nd)
+        {
+            NguoiDung tmp = db.NguoiDungs.Where(m => m.TaiKhoan == nd.TaiKhoan).First(); 
+            if ( tmp.MatKhau.Equals(nd.MatKhau))
+            {
+                return RedirectToAction("Index", "HomePage");
+            }
+            return View("Index");
         }
     }
 }
